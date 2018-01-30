2e61aaf82453b9ce71b958bef3df8a0871ceb7de
Ted Dobyns <ted.ajax@gmail.com>
2018-01-29 16:00:07 -0800

    commit of script with no identifying information in it.

---
+++ p4ws.py
@@ -0,0 +1,32 @@
+import argparse
+
+p4config = ''
+
+p4port = ''
+p4user = ''
+p4ignore = '.p4ignore'
+p4host = ''
+p4client = ''
+p4passwd = ''
+p4tickets = ''
+
+parser = argparse.ArgumentParser()
+parser.add_argument("client", help="Name of the client to switch to.")
+
+args = parser.parse_args()
+p4client = args.client
+
+print('Switching p4 workspace to ' + args.client + '\n')
+
+with open(p4config, 'w') as config_file:
+    config_file.write('P4PORT={}\n'.format(p4port))
+    config_file.write('P4USER={}\n'.format(p4user))
+    config_file.write('P4IGNORE={}\n'.format(p4ignore))
+    config_file.write('P4HOST={}\n'.format(p4host))
+    config_file.write('P4CLIENT={}\n'.format(p4client))
+    config_file.write('P4PASSWD={}\n'.format(p4passwd))
+    config_file.write('P4TICKETS={}\n'.format(p4tickets))
+
+with open(p4config, 'r') as config_file:
+    print('P4CONFIG file stored at {}:'.format(p4config))
+    print(config_file.read())
