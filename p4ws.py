c84c6ac71a69a86b79b7439ca377d2a52514728b
Ted Dobyns <ted.ajax@gmail.com>
2018-07-23 13:05:11 -0700

    added server option

--- p4ws.py
+++ p4ws.py
@@ -16,9 +16,13 @@ p4tickets = ''
 
 parser = argparse.ArgumentParser()
 parser.add_argument("client", help="Name of the client to switch to.")
+parser.add_argument("-v", "--verbose", help="If true print contents of config file.", action="store_true")
+parser.add_argument("-s", "--server", help="If true clear p4host so we can connect to server repo.", action="store_true")
 
 args = parser.parse_args()
 p4client = args.client
+verbose = args.verbose
+server = args.server
 
 print('Switching p4 workspace to ' + args.client + '\n')
 
@@ -26,11 +30,15 @@ with open(p4config, 'w') as config_file:
     config_file.write('P4PORT={}\n'.format(p4port))
     config_file.write('P4USER={}\n'.format(p4user))
     config_file.write('P4IGNORE={}\n'.format(p4ignore))
-    config_file.write('P4HOST={}\n'.format(p4host))
+    if server:
+        config_file.write('P4HOST=\n')
+    else:
+        config_file.write('P4HOST={}\n'.format(p4host))
     config_file.write('P4CLIENT={}\n'.format(p4client))
     config_file.write('P4PASSWD={}\n'.format(p4passwd))
     config_file.write('P4TICKETS={}\n'.format(p4tickets))
 
 with open(p4config, 'r') as config_file:
     print('P4CONFIG file stored at {}:'.format(p4config))
-    print(config_file.read())
+    if verbose:
+        print(config_file.read())
